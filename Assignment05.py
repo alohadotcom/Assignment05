454d19405877efd2e2899eb01653ce846b4f439c
alohadotcom <rs96744@uw.edu>
2025-02-27 07:57:10 -0800

    Add files via upload

--- Assignment05.py
+++ Assignment05.py
@@ -54,56 +54,96 @@ while (True):
 
     #Input user data
     if menu_choice =='1': #This will not work if it is an integer!
-        try:
-            student_first_name = input('Enter the student\'s first name: ')
-            if not student_first_name.isalpha(): #isalpha returns True only \n
+        while True:
+            try:
+                student_first_name = input('Enter the student\'s first name: ')
+                if not student_first_name.isalpha(): #isalpha returns True only \n
                 #if all the characters in the input string are alha
-                raise ValueError('Error: Student name should be only letters')
+                    raise ValueError('Error: Student name should be only letters')
 
-        except ValueError as e:
-            print(e) # Print the exception object (typically includes the error message)
-            print('--- Technical Error Message ---')
-            print(e.__doc__) # Print the documentation string of the exception type
-            print(e.__str__()) # Print the string representation of exception
+            except ValueError as e:
+                print(e) # Print the exception object (typically includes the error message)
+                print('--- Technical Error Message ---')
+                print(e.__doc__) # Print the documentation string of the exception type
+                print(e.__str__()) # Print the string representation of exception
+
+            except Exception as e:
+                print('Error: Issue with the data you entered')
+                print('--- Technical Error Message ---')
+                print(e.__doc__)  # Print the documentation string of the exception type
+                print(e.__str__())  # Print the string representation of exception
+
+            else:
+                break
+
+        while True:
+            try:
+                student_last_name = input('Enter the student\'s last name: ')
+                if not student_last_name.isalpha():
+                    raise ValueError ('Error: Student name should be only letters')
+
+            except ValueError as e:
+                print(e) #Print the exception object (typically includes the error message)
+                print('--- Technical Error Message ---')
+                print(e.__doc__)  # Print the documentation string of the exception type
+                print(e.__str__())  # Print the string representation of exception
+
+            except Exception as e:
+                print('Error: Issue with the data you entered')
+                print('--- Technical Error Message ---')
+                print(e.__doc__)  # Print the documentation string of the exception type
+                print(e.__str__())  # Print the string representation of exception
+
+            else:
+                break
+        course_name = input('Please enter the name of course: ')
+        student_data = {'FirstName': student_first_name, 'LastName': student_last_name,\
+                        'CourseName': course_name}
+        students.append(student_data)
+        print(f'You have registered {student_first_name} {student_last_name} for {course_name}.')
+        for row in students:
+            print(row)
+        continue
+
+    #Present the current data
+    elif menu_choice == '2':
+
+        #Present the current data
+        for student in students:
+            print(f'Student {student['FirstName']} {student['LastName']} is enrolled in {student['CourseName']}')
+
+            print('-'*50)
+            continue
+
+    #Save the data to a file
+    elif menu_choice == '3':
+        try:
+            file = open(FILE_NAME, 'w')
+            json.dump(students, file)
+            file.close()
+            print('The following data was saved to file!')
+            for students in students:
+                print(f'Student {student['FirstName']} {student['LastName']} is enrolled in {student['CourseName']}')
+                      #{student['CourseName']}')
+            continue
 
         except Exception as e:
-            print('Error: Issue with the data you entered')
+            if file.closed == False:
+                file.close()
+            print('Error: Not able to write your file')
             print('--- Technical Error Message ---')
-            print(e.__doc__)  # Print the documentation string of the exception type
-            print(e.__str__())  # Print the string representation of exception
+            print(e.__doc__)
+            print(e.__str__())
 
-        else:
-            break
-
-while True:
-    try:
-        student_last_name = input('Enter the student\'s last name: ')
-        if not student_last_name.isalpha():
-            raise ValueError ('Error: Student name should be only letters')
-
-    except ValueError as e:
-        print(e) #Print the exception object (typically includes the error message)
-        print('--- Technical Error Message ---')
-        print(e.__doc__)  # Print the documentation string of the exception type
-        print(e.__str__())  # Print the string representation of exception
-
-    except Exception as e:
-        print('Error: Issue with the data you entered')
-        print('--- Technical Error Message ---')
-        print(e.__doc__)  # Print the documentation string of the exception type
-        print(e.__str__())  # Print the string representation of exception
-
+    #Stop the loop
+    elif menu_choice == '4':
+        break #out of the loop
     else:
-        break
-course_name = input('Please enter the name of course: ')
-student_data = {'FirstName': student_first_name, 'LastName': student_last_name,\
-            'CourseName': course_name}
-student.append(student_data)
-print(f'You have registered {student_first_name} {student_last_name} for {course_name}.')
-for row in students:
-    print(row)
+        print('Please only choose option 1, 2, 3, or 4')
 
-    elif menu_choice == '2':
+print('Program Ended. Good Bye')
+
+
 
 
 
